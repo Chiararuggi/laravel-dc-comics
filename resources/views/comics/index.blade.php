833e377747b3969e5346c9b92c6ed08e0b5db957
chiara.ruggi <chiara.ruggi1998@gmail.com>
2024-01-26 09:27:01 +0100

    crud: create working

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            @foreach ($comics as $comic)
+                <div class="col-3">
+                    <div class="card" style="width: 18rem;">
+                        <img src="{{$comic->image}}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">{{$comic->title}}</h5>
+                            <p class="card-text">{{$comic->description}}</p>
+                            <p class="card-text">{{$comic->type}}</p>
+                            <p class="card-text">{{$comic->price}}</p>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+@endsection
