5a43fcaf43dda8a50fe0a0d4597f6c5ddca40d27
chiara.ruggi <chiara.ruggi1998@gmail.com>
2024-01-28 19:27:17 +0100

    working delete

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -15,6 +15,12 @@
                             <p class="card-text">{{$comic->price}}</p>
                             <a href="{{ route('comics.show', $comic->id) }}" class="btn btn-primary">Show details</a>
                             <a href="{{ route('comics.edit', $comic->id) }}" class="btn btn-info">Edit</a>
+
+                            <form action="{{ route('comics.destroy', $comic->id) }}" method="POST" class="d-inline-block">
+                                @csrf
+                                @method('DELETE')
+                                <input type="submit" value="Delete" class="btn btn-danger">
+                            </form>
                         </div>
                     </div>
                 </div>
